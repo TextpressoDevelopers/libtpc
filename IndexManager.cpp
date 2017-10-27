901c6a995ef7a11ec6cba99411f83bae7720930f
Valerio Arnaboldi <valearna@caltech.edu>
2017-10-27 12:47:19 -0700

    bug fixed
    enabled indexing from cas dir without file list

--- IndexManager.cpp
+++ IndexManager.cpp
@@ -596,7 +596,8 @@ void IndexManager::create_index_from_existing_cas_dir(const string &input_cas_di
         string file_id = dir_it->path().parent_path().parent_path().filename().string()
                          + "/" + dir_it->path().parent_path().filename().string();
         if (is_regular_file(dir_it->status()) && boost::algorithm::ends_with(
-                dir_it->path().filename().string(), ".tpcas.gz") && file_list.find(file_id) != file_list.end()) {
+                dir_it->path().filename().string(), ".tpcas.gz") && (file_list.empty() ||
+                file_list.find(file_id) != file_list.end())) {
             std::string filepath(dir_it->path().string());
             if (!process_single_file(filepath, first_paper, tmp_conf)) {
                 continue;
